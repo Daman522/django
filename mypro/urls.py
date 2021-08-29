b3dbad91fa8e1665fdf45062d929bff21b955519
Daman522 <89479951+Daman522@users.noreply.github.com>
2021-08-30 00:58:23 +0530

    Update urls.py

--- mypro/urls.py
+++ mypro/urls.py
@@ -21,3 +21,30 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',register,name="register"),
 ]
+
+
+
+
+
+
+
+
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url, include
+from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns=[
+
+    path('',views.home,name="index"),
+    path('nav/',views.nav,name="nav"),
+    path('login/',views.Log,name="login"),
+    path('createaccount/',views.createaccount,name="createaccount"),
+
+
+
+    
+]
